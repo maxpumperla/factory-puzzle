a614772398997b5082b8e37a27275c1a57492119
Max Pumperla <max.pumperla@googlemail.com>
2020-06-04 16:16:14 +0200

    masking config

--- factory/config.py
+++ factory/config.py
@@ -3,16 +3,17 @@ from .util.samples import get_default_factory, get_small_default_factory
 
 SIMULATION_CONFIG = {
     "layout": "small",
-    "num_tables": 3,
-    "num_cores": 2,
-    "num_phases": 2,
+    "num_tables": 1,
+    "num_cores": 1,
+    "num_phases": 1,
     #"seed": 1337,
     "max_num_steps": 300,
     "actions": 5,
-    "observations": 27,
+    "observations": 14, #27
     "low": -1,
     "high": 10,
-    "random_init": True,
+    "random_init": False,
+    "masking": True,
 }
 
 
@@ -25,3 +26,4 @@ def factory_from_config(config):
         return get_default_factory(**config)
     else:
         raise ValueError("Choose from either 'small' or 'big'.")
+
