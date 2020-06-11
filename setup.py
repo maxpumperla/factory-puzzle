7c686e822f422514a463eb4ef28476c0738c19b0
Max Pumperla <max.pumperla@googlemail.com>
2020-06-11 16:10:36 +0200

    requirements

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setuptools.setup(
     platforms=["Linux", "OS-X", "Windows"],
     include_package_data=True,
     install_requires=[
-        "gym", "ray[rllib]", "tensorflow", "streamlit", "opencv-python", "gputil"
+        "gym", "ray[rllib]", "tensorflow", "streamlit", "opencv-python", "gputil", "deepkit"
     ],
     extras_require={
         "dev": ["black", "pre-commit", "pytest"],
